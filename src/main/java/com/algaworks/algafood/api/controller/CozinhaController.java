25dd7bcc34158f2120aad2f78f4ce3dfd9e3b409
Fabio Aguiar <fabio@example.com>
2020-03-31 19:05:05 -0300

    Manipulando a resposta HTTP com ResponseEntity

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,7 +4,9 @@ import com.algaworks.algafood.api.model.CozinhaXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +32,8 @@ public class CozinhaController {
     }
 
     @GetMapping("/{id}")
-    public Cozinha buscar(@PathVariable() Long id) {
-        return cozinhaRepository.porId(id);
+    public ResponseEntity<Cozinha> buscar(@PathVariable() Long id) {
+        Cozinha cozinha = cozinhaRepository.porId(id);
+        return ResponseEntity.ok(cozinha);
     }
 }
